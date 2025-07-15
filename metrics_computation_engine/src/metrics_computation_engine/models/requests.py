ff0fc83b8ca28dbd1f5f2f4f1bdce3181ffe06dd
k-payumo <kpayumo@cisco.com>
2025-07-15 15:33:00 -0700

    Default swagger docs (#6)
    * fix: cleanup gds
    * fix: changing default model
    
    Signed-off-by: Kevin Payumo <kpayumo@cisco.com>

--- metrics_computation_engine/src/metrics_computation_engine/models/requests.py
+++ metrics_computation_engine/src/metrics_computation_engine/models/requests.py
@@ -8,7 +8,7 @@ from pydantic import BaseModel
 
 class LLMJudgeConfig(BaseModel):
     LLM_BASE_MODEL_URL: str = "https://api.openai.com/v1"
-    LLM_MODEL_NAME: str = "gpt-4"
+    LLM_MODEL_NAME: str = "gpt-4o"
     OPENAI_API_KEY: str = "sk-..."
     CUSTOM_API_KEY: str = ""
 
@@ -20,11 +20,11 @@ class BatchTimeRange(BaseModel):
 
 class BatchConfig(BaseModel):
     time_range: Optional[BatchTimeRange] = None
-    num_sessions: Optional[int] = None
+    num_sessions: Optional[int] = 1
     app_name: Optional[str] = None
 
 
 class MetricsConfigRequest(BaseModel):
-    metrics: List[str] = ["AgentToToolInteractions", "GraphDeterminismScore"]
+    metrics: List[str] = ["AgentToToolInteractions", "ToolUtilizationAccuracy"]
     llm_judge_config: LLMJudgeConfig = LLMJudgeConfig()
     batch_config: BatchConfig = BatchConfig()
