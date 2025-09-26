da7ead9e3e821c2e5262b2516b42630ad0d175b2
Kevin Payumo <kpayumo@cisco.com>
2025-09-26 11:55:42 -0700

    feat: Upon creation of Session Entity, MCE will query the annotations table via the data access layer.
    Signed-off-by: Kevin Payumo <kpayumo@cisco.com>

---
+++ metrics_computation_engine/src/metrics_computation_engine/dal/annotations.py
@@ -0,0 +1,11 @@
+# Copyright AGNTCY Contributors (https://github.com/agntcy)
+# SPDX-License-Identifier: Apache-2.0
+
+from .client import get_api_response
+
+def get_annotations_results_by_session(session_id: str):
+    resp = get_api_response(f"/annotations")
+    for d in resp['data']:
+        if d['session_id'] == session_id:
+            return d['annotation_value']
+    return ""
--- metrics_computation_engine/src/metrics_computation_engine/dal/sessions.py
+++ metrics_computation_engine/src/metrics_computation_engine/dal/sessions.py
@@ -17,6 +17,7 @@ from metrics_computation_engine.transformers import (
     ConversationDataExtractor,
 )
 from metrics_computation_engine.logger import setup_logger
+from metrics_computation_engine.dal.annotations import get_annotations_results_by_session
 
 logger = setup_logger(__name__)
 
@@ -33,12 +34,13 @@ def build_session_entity(session_id: str, spans: List[SpanEntity]) -> SessionEnt
     populate_app_name(session)
 
     populate_conversation_data(session)
-    populate_agent_interactions(session)  # Add this line
+    populate_agent_interactions(session)
 
     populate_conversation_elements(session)
     populate_tool_calls(session)
 
     populate_e2e_attributes(session)
+    populate_ground_truth(session)
 
     return session
 
@@ -107,6 +109,8 @@ def populate_e2e_attributes(session: SessionEntity) -> None:
     session.input_query = str(input_query)
     session.final_response = str(final_response)
 
+def populate_ground_truth(session: SessionEntity) -> None:
+    session.ground_truth = get_annotations_results_by_session(session.session_id)
 
 def populate_entity_spans(session: SessionEntity) -> None:
     """Populate entity-specific spans using filters."""
