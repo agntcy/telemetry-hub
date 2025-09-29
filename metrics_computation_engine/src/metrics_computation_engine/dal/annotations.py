d534cbc5de263e64f09f1f9de6d2dc78268a2c25
Kevin Payumo <kpayumo@cisco.com>
2025-09-29 13:41:40 -0700

    feat: updating the data access layer interface to annotation/ground_truth data
    Signed-off-by: Kevin Payumo <kpayumo@cisco.com>

--- metrics_computation_engine/src/metrics_computation_engine/dal/annotations.py
+++ metrics_computation_engine/src/metrics_computation_engine/dal/annotations.py
@@ -5,8 +5,26 @@ from .client import get_api_response
 
 
 def get_annotations_results_by_session(session_id: str):
-    resp = get_api_response("/annotations")
-    for d in resp["data"]:
-        if d["session_id"] == session_id:
-            return d["annotation_value"]
+    page = 1
+    limit = 100
+
+    while True:
+        params = {"page": page, "limit": limit}
+        resp = get_api_response(f"/annotations/session/{session_id}", params=params)
+
+        data = resp.get("data", [])
+        if not data:
+            break
+
+        # TMP: Need to figure out how to best handle human labeling where each label consists of N different evaluators
+        # For now, we will just return the first annotation value
+        for d in data:
+            if d.get("session_id") == session_id:
+                return d.get("annotation_value", "")
+
+        if not resp.get("has_next", False):
+            break
+
+        page += 1
+
     return ""
