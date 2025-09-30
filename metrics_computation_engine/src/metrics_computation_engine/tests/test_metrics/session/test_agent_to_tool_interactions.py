6768ce4ae63efd9cc61c3a30108cf79d2b278a2a
k-payumo <kpayumo@cisco.com>
2025-09-30 10:19:24 -0700

    docs: Licensing update (#65)
    * feat: Licensing update
    
    Signed-off-by: Kevin Payumo <kpayumo@cisco.com>
    
    * docs: full licensing update
    
    Signed-off-by: Kevin Payumo <kpayumo@cisco.com>
    
    * style: ruff/precommit
    
    Signed-off-by: Kevin Payumo <kpayumo@cisco.com>
    
    * docs: adding the licensing field in the project data in pyproject
    
    Signed-off-by: Kevin Payumo <kpayumo@cisco.com>
    
    * style: ruff/format
    
    Signed-off-by: Kevin Payumo <kpayumo@cisco.com>
    
    ---------
    
    Signed-off-by: Kevin Payumo <kpayumo@cisco.com>

--- metrics_computation_engine/src/metrics_computation_engine/tests/test_metrics/session/test_agent_to_tool_interactions.py
+++ metrics_computation_engine/src/metrics_computation_engine/tests/test_metrics/session/test_agent_to_tool_interactions.py
@@ -1,3 +1,6 @@
+# Copyright AGNTCY Contributors (https://github.com/agntcy)
+# SPDX-License-Identifier: Apache-2.0
+
 import pytest
 from collections import Counter
 from metrics_computation_engine.metrics.session.agent_to_tool_interactions import (
